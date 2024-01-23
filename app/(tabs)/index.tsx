83180ed1fda12fa67ed8b4a210ab9604d669551d
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-01-22 22:04:37 -0700

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,65 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView, Dimensions, View } from 'react-native';
+import Mapbox, {
+  MapView,
+  SkyLayer,
+  Logger,
+  Terrain,
+  RasterDemSource,
+  Atmosphere,
+  Camera,
+} from '@rnmapbox/maps';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+Mapbox.setAccessToken('pk.eyJ1IjoiZGVtZGV2dnkiLCJhIjoiY2xycGl3b2NpMDJjaDJrbXE2d2RhODNiZSJ9.Y34dC57ZpRlz_iEmTyZ2eQ');
+Logger.setLogLevel('verbose');
 
-export default function TabOneScreen() {
+export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MapView
+        style={styles.container}
+        projection={'globe'}
+        styleURL={'mapbox://styles/mapbox/light-v11'}
+      >
+        <Camera
+          defaultSettings={{
+            zoomLevel: 10,
+            centerCoordinate: [0, 0],
+            pitch: 0
+          }}
+        />
+        <RasterDemSource
+          id="mapbox-dem"
+          url="mapbox://mapbox.mapbox-terrain-dem-v1"
+          tileSize={514}
+          maxZoomLevel={14}
+        >
+          <Atmosphere
+            style={{
+              color: 'rgb(186, 210, 235)',
+              highColor: 'rgb(36, 92, 223)',
+              horizonBlend: 0.02,
+              spaceColor: 'white',
+              starIntensity: 0.01,
+            }}
+          />
+          <SkyLayer
+            id="sky-layer"
+            style={{
+              skyType: 'atmosphere',
+              skyAtmosphereSun: [0.0, 0.0],
+              skyAtmosphereSunIntensity: 15.0,
+            }}
+          />
+          <Terrain style={{ exaggeration: 1.5 }} />
+        </RasterDemSource>
+      </MapView>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
   title: {
     fontSize: 20,
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,54 +5,35 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  const loaded = true;
+  const error = false;
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (error) {
+      throw new Error('Throw future errors from here');
     }
+  }, [error])
+
+  useEffect(() => {
+    // Hide splash screen on future asset loads from here.
+    SplashScreen.hideAsync();
   }, [loaded]);
 
   if (!loaded) {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
